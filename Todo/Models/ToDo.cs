56346f7f500fc87fa10c883b68a2e165f1f3e563
Zachary <zdean1995@gmail.com>
2023-04-09 09:03:20 -0400

    Added model and required packages

---
+++ Todo/Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string todo { get; set; } = "";
+        public Boolean Completed { get; set; } = false;
+        [DataType(DataType.DateTime)]
+        public DateTime? CompletetionDate { get; set; }
+    }
+}
